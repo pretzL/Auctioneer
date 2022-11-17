50f4d910b4489d849bc83af8ffe83a5341b19eed
pretzL <ericpretzinger@gmail.com>
2022-11-17 17:06:19 +0100

    Some initial Tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        "main-green": "#2D8C70",
+        "light-green": "#9DBFB5",
+        "dark-green": "#1D5947",
+      },
+    },
+  },
+  plugins: [],
+};
