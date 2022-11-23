0430184bcf45be5dabba6376ce4487a5d28eb372
pretzL <ericpretzinger@gmail.com>
2022-11-23 12:44:37 +0100

    Add query params and errorContainer variables

--- src/js/util/variables.mjs
+++ src/js/util/variables.mjs
@@ -1,3 +1,8 @@
 export const API_BASE_URL = "https://nf-api.onrender.com";
 export const API_LISTINGS_URL = "/api/v1/auction/listings";
 export const API_PROFILE_URL = "/api/v1/auction/profiles/";
+
+export const listingsParams = "?_seller=true&_bids=true";
+export const profileParams = "?_listings=true";
+
+export const errorContainer = document.querySelector(".error-container");
