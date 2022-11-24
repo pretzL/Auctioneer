7a8ad43b09c6bd4f2d8b38a926981ea752b1b6fe
pretzL <ericpretzinger@gmail.com>
2022-11-24 19:23:22 +0100

    Fixed card HTML not being fully clickable

--- src/js/templates/card.mjs
+++ src/js/templates/card.mjs
@@ -7,13 +7,13 @@ export const cardHTML = (data) => {
   <figure class="relative max-w-sm cursor-pointer shadow-xl bg-base-100 rounded-lg h-[300px]">
     <a href="./listing.html?id=${data.id}">
       <img class="rounded w-full h-full brightness-75" src="${data.media[0]}" alt="Image of ${data.title}">
+      <figcaption class="absolute bottom-6 px-4 text-lg text-white flex flex-col w-full">
+        <h2 class="card-title outline-black">${data.title}</h2>
+          <div class="card-actions justify-end">
+            <label class="btn bg-dark-green">${price}</label>
+          </div>
+      </figcaption>
     </a>
-    <figcaption class="absolute bottom-6 px-4 text-lg text-white flex flex-col w-full">
-      <h2 class="card-title outline-black">${data.title}</h2>
-      <div class="card-actions justify-end">
-        <label class="btn bg-dark-green">${price}</label>
-      </div>
-    </figcaption>
   </figure>
       `;
 };
