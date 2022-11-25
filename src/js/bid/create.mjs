46f9cb8e1c3ba11fc508b9df821eb5413e98a4fe
pretzL <ericpretzinger@gmail.com>
2022-11-25 20:59:20 +0100

    Created add bid function, fixed an issue with listing description not being sent due to bad name

--- src/js/bid/create.mjs
+++ src/js/bid/create.mjs
@@ -1,7 +1,6 @@
 import { errorMessage } from "../components/error.mjs";
 import { successMessage } from "../components/success.mjs";
 import * as storage from "../storage/index.mjs";
-import { timeout } from "../util/timeout.mjs";
 import { API_BASE_URL, API_LISTINGS_URL } from "../util/variables.mjs";
 
 /**
@@ -9,41 +8,32 @@ import { API_BASE_URL, API_LISTINGS_URL } from "../util/variables.mjs";
  * @param {element} form
  * @example
  * ```js
- * createListing(listingForm);
- * // Expect a post to be created using the form input fields provided in the postForm element, if it is a form.
+ * createComment(commentsForm);
+ * // Expect a comment to be created using the form input fields provided in the commentsForm element, if it is a form.
  * ```
  */
-export async function createListing(evt) {
+export async function createBid(evt) {
   evt.preventDefault();
 
   // Grab error container
-  const errorContainer = evt.target.querySelector(".error-container");
+  const errorContainer = document.querySelector(".error-container");
 
   // Assign the inputs from the form to variables
-  const [title, desc, tags, endsAt, media] = evt.target.elements;
+  const [id, amount] = evt.target.elements;
 
-  // Remove whitespace from tags input and split them at commas
-  const pushedTags = tags.value.replace(/\s+/g, "").split(",");
+  const amountNum = Number(amount.value);
 
   // Construct the data object which is to be sent to the API
   let dataObj = {
-    title: `${title.value}`,
-    description: `${desc.value}`,
-    tags: pushedTags,
-    media: [`${media.value}`],
-    endsAt: `${endsAt.value}`,
+    amount: amountNum,
   };
-
-  if (!media.value || media.value === "") {
-    delete dataObj.media;
-  }
 
   // Get the auth token
   const jwt = storage.load("jwt");
 
   // Send the data object to the API
   try {
-    const response = await fetch(`${API_BASE_URL}${API_LISTINGS_URL}`, {
+    const response = await fetch(`${API_BASE_URL}${API_LISTINGS_URL}/${id.value}/bids`, {
       method: "POST",
       body: JSON.stringify(dataObj),
       headers: {
@@ -51,14 +41,11 @@ export async function createListing(evt) {
         "Content-Type": "application/json; charset=utf-8",
       },
     });
-
     const json = await response.json();
-
     if (json.errors) {
       errorContainer.innerHTML = errorMessage(json.errors[0].message);
     } else {
-      errorContainer.innerHTML = successMessage("Post creation");
-      timeout(2000);
+      errorContainer.innerHTML = successMessage("Bid ");
       location.reload();
     }
   } catch (error) {
