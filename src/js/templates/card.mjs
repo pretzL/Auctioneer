7af3d439f59dbf3e53debfdf4d010c857c3447b5
pretzL <ericpretzinger@gmail.com>
2022-11-25 21:09:53 +0100

    Added image validation for content

--- src/js/templates/card.mjs
+++ src/js/templates/card.mjs
@@ -1,4 +1,5 @@
 import { sortAmountAsc } from "../components/filters/amountFilter.mjs";
+import { validateImage } from "../components/validateImage.mjs";
 
 export const cardHTML = (data) => {
   let price = 0;
@@ -6,10 +7,12 @@ export const cardHTML = (data) => {
     const sorted = sortAmountAsc(data.bids);
     price = sorted[0].amount;
   }
+
+  const image = validateImage(data.media[0]);
   return `
       <figure class="relative max-w-sm cursor-pointer shadow-xl bg-base-100 rounded-lg h-[300px]">
         <a href="./listing.html?id=${data.id}">
-          <img class="rounded w-full h-full brightness-75" src="${data.media[0]}" alt="Image of ${data.title}">
+          <img class="rounded w-full h-full brightness-75" src="${image}" alt="Image of ${data.title}">
           <figcaption class="absolute bottom-6 px-4 text-lg text-white flex flex-col w-full">
             <h2 class="card-title outline-black">${data.title}</h2>
               <div class="card-actions justify-end">
