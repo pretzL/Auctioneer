428da0905cd3a2bcb57a0787a0d26d4aa0a144bc
pretzL <ericpretzinger@gmail.com>
2022-11-29 11:59:50 +0100

    Searchbar on user profile now works and updates search in real-time

--- src/js/pages/listings.mjs
+++ src/js/pages/listings.mjs
@@ -1,13 +1,17 @@
 import { search } from "../query/search.mjs";
 import { cardHTML } from "../templates/card.mjs";
 import { cardsContainer } from "../util/variables.mjs";
+import { getListings } from "../listings/read.mjs";
+import { options } from "../util/options.mjs";
+import { API_BASE_URL, API_LISTINGS_URL, listingsParams } from "../util/variables.mjs";
 
 export async function buildListings(query) {
-  const data = await search(query);
+  const data = await getListings(`${API_BASE_URL}${API_LISTINGS_URL}${listingsParams}`, options);
+  const result = await search(data, query);
 
   cardsContainer.innerHTML = "";
 
-  for (let i = 0; i < data.length; i++) {
-    cardsContainer.innerHTML += cardHTML(data[i]);
+  for (let i = 0; i < result.length; i++) {
+    cardsContainer.innerHTML += cardHTML(result[i]);
   }
 }
--- src/js/query/search.mjs
+++ src/js/query/search.mjs
@@ -1,19 +1,14 @@
-import { getListings } from "../listings/read.mjs";
-import { options } from "../util/options.mjs";
-import { API_BASE_URL, API_LISTINGS_URL, listingsParams } from "../util/variables.mjs";
-
 /**
  * Handles the page's search function. The value argument is the search field input which is then checked against the results from the API to find matches.
+ * @param {string} data data to search through
  * @param {string} value search field input
  * @example
  * ```js
- * search(test)
+ * search(apiResult, test)
  * // Expect return to show every post which has the string "test" in it somewhere
  * ```
  */
-export async function search(value) {
-  const data = await getListings(`${API_BASE_URL}${API_LISTINGS_URL}${listingsParams}`, options);
-
+export async function search(data, value) {
   const filtered = data.filter((listing) => {
     let desc;
     if (listing.description) {
