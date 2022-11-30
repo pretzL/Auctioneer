550074cd47f65891f9a4be6568e256b3b3dda920
pretzL <ericpretzinger@gmail.com>
2022-11-30 19:29:34 +0100

    Fix long emails looking bad on user profile and listing page

--- src/js/templates/sellerInfo.mjs
+++ src/js/templates/sellerInfo.mjs
@@ -4,7 +4,7 @@ export const buildSellerInfo = (data) => {
       <img src="${data.seller.avatar}" class="rounded-full w-24 h-24 object-cover object-center" alt="${data.seller.name}'s profile image"/>
       <div id="seller-info-text" class="flex flex-col gap-2">
         <h3 class="text-lg font-semibold">${data.seller.name}</h3>
-        <p>${data.seller.email}</p>
+        <p class="text-xs lg:text-base">${data.seller.email}</p>
         <p>Wins: ${data.seller.wins.length}</p>
       </div>
     </a>
--- src/js/templates/userInfoCard.mjs
+++ src/js/templates/userInfoCard.mjs
@@ -3,13 +3,13 @@ export const userInfoCard = (data) => {
   <div id="info-text" class="w-full">
     <div class="w-full flex flex-col gap-4">
       <h1 class="font-bold mb-4 text-lg">${data.name}</h1>
-      <p>${data.email}</p>
+      <p class="text-xs lg:text-base">${data.email}</p>
       <p>Credits: ${data.credits}</p>
       <p>Listings: ${data._count.listings}</p>
     </div>
   </div>
   <div>
-    <label for="edit-media-modal" class="tooltip flex flex-col w-32 h-32 btn btn-ghost btn-circle" data-tip="Click to edit">
+    <label for="edit-media-modal" class="tooltip flex flex-col w-24 h-24 md:w-32 md:h-32 btn btn-ghost btn-circle" data-tip="Click to edit">
       <img id="edit-profile-media" src="${data.avatar}" class="rounded-full w-32 h-32 self-end object-cover object-center" alt="${data.name}'s profile image"/>
     </label>
   </div>
