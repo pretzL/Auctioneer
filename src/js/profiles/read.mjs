95c0180d506160bb4b3f3f31fd29ccd55d7dd4e3
pretzL <ericpretzinger@gmail.com>
2022-11-30 15:55:07 +0100

    Rewrite profiles/read.mjs, fixed problem with card rendering if bids info is missing

--- src/js/profiles/read.mjs
+++ src/js/profiles/read.mjs
@@ -2,24 +2,8 @@ import { options } from "../util/options.mjs";
 import { API_BASE_URL, API_PROFILE_URL, profileParams, listingsParams } from "../util/variables.mjs";
 
 // Get a single profile
-export async function getProfile(name) {
-  const response = await fetch(`${API_BASE_URL}${API_PROFILE_URL}/${name}${profileParams}`, options);
-  const data = await response.json();
-
-  return data;
-}
-
-// Get the user's listings
-export async function getProfileListings(name) {
-  const response = await fetch(`${API_BASE_URL}${API_PROFILE_URL}/${name}/listings${listingsParams}`, options);
-  const data = await response.json();
-
-  return data;
-}
-
-// Get the user's bids
-export async function getBids(name) {
-  const response = await fetch(`${API_BASE_URL}${API_PROFILE_URL}/${name}/bids${profileParams}`, options);
+export async function getProfile(name, type) {
+  const response = await fetch(`${API_BASE_URL}${API_PROFILE_URL}/${name}${type}${profileParams}`, options);
   const data = await response.json();
 
   return data;
