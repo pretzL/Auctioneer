e83b5ad6513f253a353f1def6187a24a71bfa879
pretzL <ericpretzinger@gmail.com>
2022-12-01 12:24:42 +0100

    Added handling for empty string avatars

--- src/js/templates/sellerInfo.mjs
+++ src/js/templates/sellerInfo.mjs
@@ -1,7 +1,11 @@
+import { validateAvatar } from "../components/validateAvatar.mjs";
+
 export const buildSellerInfo = (data) => {
+  const avatar = validateAvatar(data.seller.avatar);
+
   return `
     <a href="./profile.html?name=${data.seller.name}" class="max-w-sm flex gap-4 items-center">
-      <img src="${data.seller.avatar}" class="rounded-full w-24 h-24 object-cover object-center" alt="${data.seller.name}'s profile image"/>
+      <img src="${avatar}" class="rounded-full w-24 h-24 object-cover object-center" alt="${data.seller.name}'s profile image"/>
       <div id="seller-info-text" class="flex flex-col gap-2">
         <h3 class="text-lg font-semibold">${data.seller.name}</h3>
         <p class="text-xs lg:text-base">${data.seller.email}</p>
