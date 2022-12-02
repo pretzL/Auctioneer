217b9eac9530c1a87a7d4c0aa971ed257630f2e7
pretzL <ericpretzinger@gmail.com>
2022-12-02 15:37:41 +0100

    Fixed user profile image looking rather wonky on profile page

--- src/js/templates/userInfoCard.mjs
+++ src/js/templates/userInfoCard.mjs
@@ -3,14 +3,14 @@ import { userInfo } from "../util/variables.mjs";
 export const userInfoCard = (data) => {
   let avatar = `
     <label class="flex flex-col w-24 h-24 md:w-32 md:h-32 btn btn-ghost btn-circle">
-      <img id="edit-profile-media" src="${data.avatar}" class="rounded-full w-32 h-32 self-end object-cover object-center" alt="${data.name}'s profile image"/>
+      <img id="edit-profile-media" src="${data.avatar}" class="rounded-full w-full h-full self-end object-cover object-center" alt="${data.name}'s profile image"/>
     </label>
   `;
 
-  if (data.avatar === userInfo.name) {
+  if (data.name === userInfo.name) {
     avatar = `
-    <label for="edit-media-modal" class="tooltip flex flex-col w-24 h-24 md:w-32 md:h-32 btn btn-ghost btn-circle" data-tip="Click to edit">
-      <img id="edit-profile-media" src="${data.avatar}" class="rounded-full w-32 h-32 self-end object-cover object-center" alt="${data.name}'s profile image"/>
+    <label for="edit-media-modal" class="tooltip flex flex-col w-24 h-24 md:w-32 md:h-32 rounded-full" data-tip="Click to edit">
+      <img id="edit-profile-media" src="${data.avatar}" class="rounded-full w-full h-full self-end object-cover object-center" alt="${data.name}'s profile image"/>
     </label>
     `;
   }
@@ -24,7 +24,7 @@ export const userInfoCard = (data) => {
       <p>Listings: ${data._count.listings}</p>
     </div>
   </div>
-  <div>
+  <div class="w-full h-full flex justify-end">
     ${avatar}
   </div>
   `;
