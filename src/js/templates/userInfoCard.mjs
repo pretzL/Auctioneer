d6adf429054eed12a1ef45d7264633bdc388989b
pretzL <ericpretzinger@gmail.com>
2022-12-02 16:16:38 +0100

    Add inm-line error handling for images

--- src/js/templates/userInfoCard.mjs
+++ src/js/templates/userInfoCard.mjs
@@ -3,14 +3,14 @@ import { userInfo } from "../util/variables.mjs";
 export const userInfoCard = (data) => {
   let avatar = `
     <label class="flex flex-col w-24 h-24 md:w-32 md:h-32">
-      <img id="edit-profile-media" src="${data.avatar}" class="rounded-full w-full h-full self-end object-cover object-center" alt="${data.name}'s profile image"/>
+      <img id="edit-profile-media" src="${data.avatar}" class="rounded-full w-full h-full self-end object-cover object-center" alt="${data.name}'s profile image" onerror='this.src="https://cdn.discordapp.com/attachments/931268688412299274/1026475078847823972/Hero-Banner-Placeholder-Dark-1024x480-1.png"'/>
     </label>
   `;
 
   if (data.name === userInfo.name) {
     avatar = `
     <label for="edit-media-modal" class="tooltip flex flex-col w-24 h-24 md:w-32 md:h-32 rounded-full cursor-pointer" data-tip="Click to edit">
-      <img id="edit-profile-media" src="${data.avatar}" class="rounded-full w-full h-full self-end object-cover object-center" alt="${data.name}'s profile image"/>
+      <img id="edit-profile-media" src="${data.avatar}" class="rounded-full w-full h-full self-end object-cover object-center" alt="${data.name}'s profile image" onerror='this.src="https://cdn.discordapp.com/attachments/931268688412299274/1026475078847823972/Hero-Banner-Placeholder-Dark-1024x480-1.png"'/>
     </label>
     `;
   }
