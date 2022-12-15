02c76dd5fbc2bf0dbb28f7a730f52207d797768e
pretzL <ericpretzinger@gmail.com>
2022-12-15 11:25:37 +0100

    Fixed issue where filtering on listings.html page would only return 9 results

--- src/js/pages/index/sortListeners.mjs
+++ src/js/pages/index/sortListeners.mjs
@@ -16,34 +16,38 @@ import { aToZSort, cardsContainer, endsAtSort, monthSort, timeSortButton, todayS
  * ```
  */
 export function addSortListener(data) {
+  let num;
+  if (location.href.includes("index")) {
+    num = 9;
+  }
   timeSortButton.addEventListener("click", () => {
     const sorted = sortTimeAsc(data);
     cardsContainer.innerHTML = "";
-    loopCardData(sorted, 9);
+    loopCardData(sorted, num);
   });
   aToZSort.addEventListener("click", () => {
     const sorted = sortTitleDesc(data);
     cardsContainer.innerHTML = "";
-    loopCardData(sorted, 9);
+    loopCardData(sorted, num);
   });
   zToASort.addEventListener("click", () => {
     const sorted = sortTitleAsc(data);
     cardsContainer.innerHTML = "";
-    loopCardData(sorted, 9);
+    loopCardData(sorted, num);
   });
   todaySort.addEventListener("click", () => {
     const sorted = sortToday(data);
     cardsContainer.innerHTML = "";
-    loopCardData(sorted, 9);
+    loopCardData(sorted, num);
   });
   monthSort.addEventListener("click", () => {
     const sorted = sortMonth(data);
     cardsContainer.innerHTML = "";
-    loopCardData(sorted, 9);
+    loopCardData(sorted, num);
   });
   endsAtSort.addEventListener("click", () => {
     const sorted = sortEndsAtDesc(data);
     cardsContainer.innerHTML = "";
-    loopCardData(sorted, 9);
+    loopCardData(sorted, num);
   });
 }
