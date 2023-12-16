a9750e3e2dac98274d13f75da7dff6e2f911ec34
pretzL <ericpretzinger@gmail.com>
2023-12-16 23:04:00 +0100

    Add time related utils functions

--- src/js/utils/index.js
+++ src/js/utils/index.js
@@ -1,3 +1,6 @@
 export * from "./clear.js";
 export * from "./createNewElement.js";
 export * from "./api.js";
+export * from "./orderBy.js";
+export * from "./timeAgo.js";
+export * from "./timeUntil.js";
